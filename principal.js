11f3884e871ee4374a99426ef8d462411730f972
Brito01 <126882124+Brito01@users.noreply.github.com>
2023-09-01 09:59:12 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -11,12 +11,22 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
+var pesoEhValido = true;
+var alturaEhValido = true;
 
-if(peso < 0 > 1000){
-    console.log("Peso inválido");
+if(peso <= 0 || peso => 1000){
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválida";
 }
 
+if(altura <= 0 || altura => 3.00){
+    console.log("Altura inválida!");
+    alturaEhValido = false;
+    tdImc.textContent = "Altura inválida";
+}
 
-
-var imc = peso / (altura * altura) ;
+if (alturaEhValido && pesoEhValido) {
+    var imc = peso / (altura * altura) ;
 tdImc.textContent = imc;
+}
