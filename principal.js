c515dd0379c0c3ffb350b2f53529012b53589185
Brito01 <126882124+Brito01@users.noreply.github.com>
2023-09-05 11:44:49 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,7 +1,11 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparencida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length ; i++ ){
+
+var paciente = pacientes[i];
 
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
@@ -14,19 +18,29 @@ var tdImc = paciente.querySelector(".info-imc");
 var pesoEhValido = true;
 var alturaEhValido = true;
 
-if(peso <= 0 || peso => 1000){
+if(peso <= 0 || peso >= 1000){
     console.log("Peso inválido!");
     pesoEhValido = false;
-    tdImc.textContent = "Peso inválida";
+    tdImc.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
 }
 
-if(altura <= 0 || altura => 3.00){
+if(altura <= 0 || altura >= 3.00){
     console.log("Altura inválida!");
     alturaEhValido = false;
     tdImc.textContent = "Altura inválida";
-}
+    paciente.classList.add("paciente-invalido");
+   }
 
 if (alturaEhValido && pesoEhValido) {
     var imc = peso / (altura * altura) ;
-tdImc.textContent = imc;
+tdImc.textContent = imc.toFixed(2);
+   }
+}
+
+titulo.addEventKListener("click, mostraMensagem")
+
+
+function mostraMensagem(){
+    console.log("Olá eu fui clicado!");
 }
