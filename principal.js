2ff79ca5c47832ed27657a1902f8a5b1a78d5f67
Brito01 <126882124+Brito01@users.noreply.github.com>
2023-09-15 08:52:47 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,49 +1,49 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparencida Nutricionista";
+titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i < pacientes.length ; i++ ){
+for (var i = 0; i < pacientes.length; i++) {
 
-var paciente = pacientes[i];
+    var paciente = pacientes[i];
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-var pesoEhValido = true;
-var alturaEhValido = true;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
-    paciente.classList.add("paciente-invalido");
-}
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
 
-if(altura <= 0 || altura >= 3.00){
-    console.log("Altura inválida!");
-    alturaEhValido = false;
-    tdImc.textContent = "Altura inválida!";
-    paciente.classList.add("paciente-invalido");
-   }
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
 
-if(alturaEhValido && pesoEhValido){
-    var imc = peso / (altura * altura) ;
-    tdImc.textContent = imc.toFixed(2);
-   }
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("clink",function(event){
+botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    
+
     var form = document.querySelector("#form-adiciona");
-    
+
     var nome = form.nome.value;
     var peso = form.peso.value;
     var altura = form.altura.value;
@@ -72,6 +72,3 @@ botaoAdicionar.addEventListener("clink",function(event){
     tabela.appendChild(pacienteTr);
 
 });
-
-
-//
